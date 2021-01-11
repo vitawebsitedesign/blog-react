33770493c9f1067853b98ce18664f2b572fadd39
Michael Nguyen <vitawebsitedesign@users.noreply.github.com>
2021-01-11 21:49:46 +1100

    add exhibit assets

--- src/components/Exhibit.js
+++ src/components/Exhibit.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
+import Gist from 'super-react-gist';
 import {getExhibit} from '../redux/selectors/exhibits';
 import ExhibitUrls from './ExhibitUrls';
+import HtmlVideo from './HtmlVideo';
+import './Exhibit.css';
 
 const getOverviewParagraphs = overview => overview.trim().split('\n').map((o, idx) => 
     <p key={idx}>
@@ -14,6 +17,41 @@ const getLearntListItems = lessons => lessons.map((l, idx) =>
     <li key={idx}>
         {l}
     </li>
+);
+
+const getPreviewImgs = imgs => imgs.map((img, idx) => 
+    <a key={idx} href={img.src} alt="Click to view this full-size image in a new tab" target="_blank" className="col-12 col-lg-6">
+        <figure>
+            <img src={img.src} alt={img.alt} className="w-100 wrapper-assets--photo" />
+            <figcaption className="p-2 text-small">{img.alt}</figcaption>
+        </figure>
+    </a>
+);
+
+const getPreviewVids = vids => vids.map((v, idx) =>
+    <div key={idx} className="col-12 col-lg-6">
+        <HtmlVideo mp4s={[v.mp4]} />
+    </div>
+);
+
+const getPreviewYoutube = youtubes => youtubes.map((url, idx) =>
+    <iframe
+        key={idx}
+        src={url}
+        title={`Embedded youtube video for ${url}`}
+        width="560"
+        height="315"
+        frameBorder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+        className="col-12 col-lg-6"
+    />
+);
+
+const getEmbeddedCodeSnippets = urls => urls && urls.map((u, idx) => 
+    <div key={idx} className="col-12 gist-container">
+        <Gist url={u} />
+    </div>
 );
 
 class Exhibit extends React.Component {
@@ -31,7 +69,7 @@ class Exhibit extends React.Component {
                     <div className="col-12 page-edge-shadow"></div>
                 </div>
                 <div className="row App--row">
-                    <aside className="col-12 col-xxl-4 d-flex">
+                    <aside className="col-12 col-xl-4 d-flex">
                     <aside className="d-none d-xl-flex bookmark flex-column mr-5">
                         <div>
                             <div className="d-inline-block bookmark--pagenum">
@@ -45,10 +83,12 @@ class Exhibit extends React.Component {
                     <aside className="d-flex flex-column flex-grow-1 ml-4 text-left wrapper-textual">
                         <div className="d-flex flex-column text-uppercase text-lighter wrapper-textual--title">
                             <div className="text-small text-uppercase">
-                                <Link to="/" className="text-bold">home</Link> &gt; exhibit
+                                <Link to="/" className="text-bold">
+                                    home
+                                </Link> &gt; exhibit
                             </div>
                             <div className="mt-1 mb-3">
-                                <div className="text-powder-blue">{this.props.item.title}</div>
+                                <div className="text-break text-powder-blue wrapper-textual--title--text">{this.props.item.title}</div>
                             </div>
                         </div>
                         <div className="mt-5 mb-3 divider"></div>
@@ -57,7 +97,7 @@ class Exhibit extends React.Component {
                                 {this.props.item.preview.about}
                             </div>
                         </div>
-                        <div className="d-flex mb-5">
+                        <div className="d-flex mb-5 flex-wrap">
                             {tagEles}
                         </div>
                         <section className="mt-4 row wrapper-textual--article">
@@ -80,12 +120,17 @@ class Exhibit extends React.Component {
                         </section>
     
                         <section className="mt-5 mb-5 wrapper-textual--table-of-contents">
-                        <ExhibitUrls urlsByType={this.props.item.urlsByType} />
+                            <ExhibitUrls urlsByType={this.props.item.urlsByType} />
                         </section>
                     </aside>
                     </aside>
-                    <main className="col-12 col-xxl-8 wrapper-gallery">
-    
+                    <main className="col-12 col-xl-7 offset-xl-1 wrapper-gallery wrapper-assets">
+                        <div className="row">
+                            {this.props.item.codeSnippets && this.props.item.codeSnippets.length && getEmbeddedCodeSnippets(this.props.item.codeSnippets)}
+                            {this.props.item.preview.images && getPreviewImgs(this.props.item.preview.images)}
+                            {this.props.item.preview.videos && getPreviewVids(this.props.item.preview.videos)}
+                            {this.props.item.preview.youtubes && getPreviewYoutube(this.props.item.preview.youtubes)}
+                        </div>
                     </main>
                 </div>
             </div>
--- src/components/GalleryTileCaption.js
+++ src/components/GalleryTileCaption.js
@@ -2,7 +2,7 @@ import './GalleryTileCaption.css';
 
 const GalleryTileCaption = props => {
     return (
-        <div className={props.className + " w-100 p-2 position-absolute text-uppercase text-left caption"}>
+        <div className={props.className + " p-2 position-absolute text-uppercase text-left caption"}>
             {props.captionTitle}
         </div>
     );
